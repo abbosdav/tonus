86a0c997127e329ce3d85bd48f430ecefa96f37b
Пользователь <abbos.d97@gmail.com>
2023-04-20 22:58:41 +0500

    fourth commit

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true,
-  publicPath:'/gym-admin/'
+  base:'/gym-admin/',
+  transpileDependencies: true
 })
